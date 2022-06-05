7d055049ff011fc74bac92e6b3b845316b607ba0
kad1kad <k.aliu@me.com>
2022-06-05 14:38:36 +0100

    feat chord change functionality

--- components/ChordProgression.js
+++ components/ChordProgression.js
@@ -1,18 +1,53 @@
 import { Key } from "@tonaljs/tonal";
 import styles from "./ChordProgression.module.scss";
+import { useState } from "react";
 
 function ChordProgression() {
-  const keyChords = Key.majorKey("C");
+  const startNotes = [
+    "C",
+    "C#",
+    "D",
+    "D#",
+    "Eb",
+    "E",
+    "F",
+    "F#",
+    "G",
+    "G#",
+    "A",
+    "A#",
+    "B",
+  ];
+
+  function handleChange(e) {
+    const note = e.target.value;
+    setKeyNote(note);
+  }
+
+  const [keyNote, setKeyNote] = useState(startNotes[0]);
+
+  const keyChords = Key.majorKey(keyNote);
 
   console.log(keyChords);
-
   return (
     <div className={styles.chordsContainer}>
-      <h2>Chords in the key of {keyChords.tonic}</h2>
+      <label htmlFor="start-note">Chords in the key of {keyChords.tonic}</label>
+
+      <div className="custom-select">
+        <select name="start-note" id="start-note" onChange={handleChange}>
+          {startNotes.map((startNote, i) => (
+            <option key={i} value={startNote}>
+              {startNote}
+            </option>
+          ))}
+        </select>
+      </div>
+
       {keyChords.chords.map((chord, i) => (
         <div key={i}>
           <button className={styles.chordBtn}>
-            {keyChords.grades[i]} {chord}
+            <span>{keyChords.grades[i]} </span>
+            <span>{chord}</span>
           </button>
         </div>
       ))}
