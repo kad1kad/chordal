6693122b02fab9f59809a6386ba035dedbf2756b
kad1kad <k.aliu@me.com>
2022-06-06 15:44:23 +0100

    comment out chord-progression component

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import ChordProgression from "../components/ChordProgression";
+import Modes from "../components/Modes";
 
 export default function Home() {
   return (
@@ -14,7 +15,8 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <ChordProgression />
+      {/* <ChordProgression /> */}
+      <Modes />
     </div>
   );
 }
