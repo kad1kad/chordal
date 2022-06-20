3a862cff2a8bb27d155635eeab2921d7c11fbacb
kad1kad <k.aliu@me.com>
2022-06-20 11:59:12 +0100

    rename octave variable; add chord detect to solve enharmonic chords

--- components/ChordButton.js
+++ components/ChordButton.js
@@ -1,24 +1,30 @@
-import React, {useMemo} from 'react';
-import {Chord, Note} from "@tonaljs/tonal";
+import { useMemo } from "react";
+import { Chord, Note } from "@tonaljs/tonal";
 
-export const ChordButton = ({setNotes, chord}) => {
-  const chordNames = useMemo(() => Chord.get(chord).notes.map((note, index) => {
-    const addValue = index < 3 ? "3" : "4";
+export const ChordButton = ({ setNotes, chord }) => {
+  const chordNames = useMemo(
+    () =>
+      Chord.get(chord).notes.map((note, index) => {
+        const addOctave = index < 3 ? "3" : "4";
 
-    return note.endsWith("##") ? Note.simplify(note + addValue) : note + addValue;
-  }), [chord]);
+        return note.endsWith("##")
+          ? Note.simplify(note + addOctave)
+          : note + addOctave;
+      }),
+    [chord]
+  );
 
   const handleMouseDown = () => {
-      setNotes([
-        {
-          name: chordNames
-        },
-      ])
-  }
+    setNotes([
+      {
+        name: chordNames,
+      },
+    ]);
+  };
 
   const handleMouseUp = () => {
     setNotes(null);
-  }
+  };
 
   return (
     <button
@@ -27,7 +33,7 @@ export const ChordButton = ({setNotes, chord}) => {
       onMouseUp={handleMouseUp}
       onTouchEnd={handleMouseUp}
     >
-      <span>{chord}</span>
+      <span>{Chord.detect(chordNames)[0]}</span>
 
       {/* Remove double sharps in single notes */}
 
@@ -37,7 +43,7 @@ export const ChordButton = ({setNotes, chord}) => {
         </span>
       ))}
     </button>
-  )
-}
+  );
+};
 
 export default ChordButton;
