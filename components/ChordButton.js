92bd60ffbbfd8de94c802a98cd6b55e50e6731b5
kad1kad <k.aliu@me.com>
2022-06-21 19:00:47 +0100

    rename chordNames variable

--- components/ChordButton.js
+++ components/ChordButton.js
@@ -2,7 +2,7 @@ import { useMemo } from "react";
 import { Chord, Note } from "@tonaljs/tonal";
 
 export const ChordButton = ({ setNotes, chord }) => {
-  const chordNames = useMemo(
+  const chordNotes = useMemo(
     () =>
       Chord.get(chord).notes.map((note, index) => {
         const addOctave = index < 3 ? "3" : "4";
@@ -17,7 +17,7 @@ export const ChordButton = ({ setNotes, chord }) => {
   const handleMouseDown = () => {
     setNotes([
       {
-        name: chordNames,
+        name: chordNotes,
       },
     ]);
   };
@@ -33,7 +33,7 @@ export const ChordButton = ({ setNotes, chord }) => {
       onMouseUp={handleMouseUp}
       onTouchEnd={handleMouseUp}
     >
-      <span>{Chord.detect(chordNames)[0]}</span>
+      <span>{Chord.detect(chordNotes)[0]}</span>
 
       {/* Remove double sharps in single notes */}
 
